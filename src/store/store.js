33085185788b9236440f2af4faedfb23c91dc30a
kripi-png <66375160+kripi-png@users.noreply.github.com>
2022-07-15 17:15:38 +0300

    store values for selected character

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { writable } from 'svelte/store';
 
 const storedWeapons = localStorage.selectedWeapons;
 const storedItems = localStorage.selectedItems;
+const storedCharacter = localStorage.selectedCharacter;
 
 const defaultValue = Array(6).fill(null);
 export const selectedWeapons = writable(
@@ -10,10 +11,16 @@ export const selectedWeapons = writable(
 export const selectedItems = writable(
   storedItems ? JSON.parse(storedItems) : defaultValue
 );
+export const selectedCharacter = writable(
+  storedCharacter ? JSON.parse(storedCharacter) : ''
+);
 
 selectedWeapons.subscribe(
   (value) => (localStorage.selectedWeapons = JSON.stringify(value))
 );
 selectedItems.subscribe(
   (value) => (localStorage.selectedItems = JSON.stringify(value))
+);
+selectedCharacter.subscribe(
+  (value) => (localStorage.selectedCharacter = JSON.stringify(value))
 );
