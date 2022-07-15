606a8b5c3762548b346778695cb9b9ae6dcf98ed
kripi-png <66375160+kripi-png@users.noreply.github.com>
2022-07-15 15:53:17 +0300

    save and load selected weaponst to/from localStorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,19 @@
 import { writable } from 'svelte/store';
 
-export const selectedWeapons = writable(Array(6).fill(null));
-export const selectedItems = writable(Array(6).fill(null));
+const storedWeapons = localStorage.selectedWeapons;
+const storedItems = localStorage.selectedItems;
+
+const defaultValue = Array(6).fill(null);
+export const selectedWeapons = writable(
+  storedWeapons ? JSON.parse(storedWeapons) : defaultValue
+);
+export const selectedItems = writable(
+  storedItems ? JSON.parse(storedItems) : defaultValue
+);
+
+selectedWeapons.subscribe(
+  (value) => (localStorage.selectedWeapons = JSON.stringify(value))
+);
+selectedItems.subscribe(
+  (value) => (localStorage.selectedItems = JSON.stringify(value))
+);
