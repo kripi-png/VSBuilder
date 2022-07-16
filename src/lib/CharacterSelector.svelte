f19dd160ecd6c974af3432c3f790031e10031232
kripi-png <66375160+kripi-png@users.noreply.github.com>
2022-07-16 23:38:19 +0300

    character selector

---
+++ src/lib/CharacterSelector.svelte
@@ -0,0 +1,40 @@
+<script>
+  import CHARACTER_DATA from '/src/assets/characters.json';
+
+  import { selectedWeapons } from '/src/store/store.js';
+
+  export let selectedCharacter = {};
+
+  const selectCharacter = (characterId) => {
+    // cancel if character is already selected
+    if ($selectedCharacter.id === characterId) return;
+    // save selection
+    selectedCharacter.set(CHARACTER_DATA[characterId]);
+    const newWeapons = [...$selectedWeapons];
+    // set starter weapon
+    newWeapons[0] = $selectedCharacter.startingWeapon.id;
+    selectedWeapons.set(newWeapons);
+  };
+</script>
+
+<div class="modalWrapper">
+  <h1 class="text-3xl font-bold mb-5">Character Selection</h1>
+  <div class="flex flex-row flex-wrap gap-2">
+    {#each Object.entries(CHARACTER_DATA) as [id, { name, image }]}
+      <div
+        class="border-4 border-solid
+        {$selectedCharacter.id === id
+          ? 'bg-green-400 border-green-600'
+          : 'bg-amber-200 border-amber-400'}"
+      >
+        <img
+          on:click={() => selectCharacter(id)}
+          src={image}
+          alt={name}
+          width="100"
+          height="100"
+        />
+      </div>
+    {/each}
+  </div>
+</div>
--- src/lib/ChracterBox.svelte
+++ src/lib/ChracterBox.svelte
@@ -1,45 +1,66 @@
 <script>
+  import { writable } from 'svelte/store';
+  import Modal, { bind } from 'svelte-simple-modal';
+
+  import CharacterSelector from './CharacterSelector.svelte';
+  import WEAPON_DATA from '/src/assets/weapons.json';
+  import CHARACTER_DATA from '/src/assets/characters.json';
+
   import { selectedCharacter, selectedWeapons } from '/src/store/store.js';
 
-  import CHARACTER_DATA from '/src/assets/characters.json';
-  import WEAPON_DATA from '/src/assets/weapons.json';
+  const modal = writable(null);
+  const showModal = () =>
+    modal.set(
+      bind(CharacterSelector, {
+        selectedCharacter,
+      })
+    );
 
   if (typeof $selectedCharacter === 'string') {
-    selectedCharacter.set(CHARACTER_DATA[$selectedCharacter]);
+    const characterId = $selectedCharacter;
+    selectedCharacter.set({
+      ...CHARACTER_DATA[characterId],
+      id: characterId,
+    });
   }
 
+  // set starter weapon
   if (selectedWeapons[0] !== $selectedCharacter.startingWeapon.id) {
     const newSelectedWeapons = [...$selectedWeapons];
     newSelectedWeapons[0] = $selectedCharacter.startingWeapon.id;
     selectedWeapons.set(newSelectedWeapons);
   }
-
-  const characterName = $selectedCharacter.name;
-  const characterImage = $selectedCharacter.image;
-  const characterStartingName = $selectedCharacter.startingWeapon.name;
-  const characterStartingImage = $selectedCharacter.startingWeapon.image;
 </script>
 
-<div
-  class="flex flex-col grow-0 mt-5 ml-5 bg-amber-200 border-4 border-amber-400 w-fit p-5"
->
-  <h1 class="text-3xl">
-    {characterName}
-  </h1>
-  <div class="relative flex flex-row">
-    <img
-      class="flex-1"
-      src={characterImage}
-      alt={characterName}
-      width="200"
-      height="200"
-    />
-    <img
-      class="absolute -right-3 -bottom-3 p-2 bg-amber-100/75 rounded"
-      src={characterStartingImage}
-      alt={characterStartingName}
-      width="100"
-      height="100"
-    />
+<div>
+  <div
+    class="
+    bg-amber-200 border-4 border-amber-400
+    flex flex-col grow-0
+    p-5 ml-5 mt-5
+    w-1/6 h-1/6
+    "
+  >
+    <h1 class="text-2xl">
+      {$selectedCharacter.name}
+    </h1>
+    <div class="relative flex flex-row" on:click={showModal}>
+      <img
+        class="flex-1"
+        src={$selectedCharacter.image}
+        alt={$selectedCharacter.name}
+        width="200"
+      />
+
+      <img
+        class="absolute -right-3 -bottom-3 p-2 bg-amber-100/75 rounded"
+        src={$selectedCharacter.startingWeapon.image}
+        alt={$selectedCharacter.startingWeapon.name}
+        width="100"
+        height="100"
+      />
+    </div>
   </div>
+
+  <Modal show={$modal} />
 </div>
