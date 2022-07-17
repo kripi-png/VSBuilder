9ba3e40ccfcd4f143e0bee28807d86640ea69c83
kripi-png <66375160+kripi-png@users.noreply.github.com>
2022-07-17 22:24:41 +0300

    component for stage item grid

---
+++ src/lib/StageItemGrid.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let stageItems = [];
+  console.log(stageItems);
+</script>
+
+<div
+  class="flex flex-col flex-wrap justify-between align-between gap-5 p-3 flex-1"
+>
+  {#each stageItems as stageItem}
+    <img
+      class="bg-amber-100/75 rounded w-24 h-24"
+      src={stageItem.image}
+      alt={stageItem.name}
+      width="96"
+      height="96"
+    />
+  {/each}
+</div>
